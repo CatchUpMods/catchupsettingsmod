f8ea5a640a0261e09734787f8e9c53afd0b247db
Tedozi Manson <duyphan.developer@gmail.com>
2017-04-20 22:02:00 +0700

    Refactor

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -9,14 +9,14 @@ if (!function_exists('cms_settings')) {
     }
 }
 
-if (!function_exists('get_settings')) {
+if (!function_exists('get_setting')) {
     /**
      * Get the available cms settings.
      *
      * @param  string|null $key
      * @return string|array
      */
-    function get_settings($key = null, $default = null)
+    function get_setting($key = null, $default = null)
     {
         return cms_settings()->getSetting($key, $default);
     }
